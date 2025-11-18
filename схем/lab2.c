ee9ea57cc5752a0853b02da74946bec85de72573
Archibald193 <arturk2k5@gmail.com>
2025-11-18 10:12:27 +0300

    Update lab2.c

--- схем/lab2.c
+++ схем/lab2.c
@@ -7,49 +7,61 @@ int main(void) {
     DDRB = 0xFF;
     DDRD = 0x00;
     PORTD = 0xFF;
-    PORTB = 0x00;
 
     uint8_t a = 0;
     uint8_t b = 0;
+    
+    uint8_t input_state = 0;
+    uint8_t current_input = 0;
+
+    PORTB = 0;
 
     while (1) {
-        if (!(PIND & (1 << 0))) {
-            a++;
-            PORTB = a;
-            _delay_ms(200);
-        }
+        current_input = ~PIND & 0b00000111;
+        
+        switch (input_state) {
+            case 0:
+                PORTB = current_input;
+                if (!(PIND & (1 << 3))) {
+                    a = current_input;
+                    input_state = 1;
+                    PORTB = 0;
+                    _delay_ms(200);
+                }
+                break;
 
-        if (!(PIND & (1 << 1))) {
-            b++;
-            PORTB = b;
-            _delay_ms(200);
-        }
+            case 1:
+                PORTB = current_input;
+                if (!(PIND & (1 << 3))) {
+                    b = current_input;
+                    input_state = 2;
+                    PORTB = 0b11111111;
+                    _delay_ms(300);
+                    PORTB = 0;
+                }
+                break;
 
-        if (!(PIND & (1 << 2))) {
-            PORTB = a + b;
+            case 2:
+                if (!(PIND & (1 << 4))) {
+                    PORTB = a + b;
+                }
+                else if (!(PIND & (1 << 5))) {
+                    PORTB = a - b;
+                }
+                else if (!(PIND & (1 << 6))) {
+                    PORTB = a * b;
+                }
+                else if (!(PIND & (1 << 7))) {
+                    if (b != 0) {
+                        PORTB = a / b;
+                    } else {
+                        PORTB = 0xFF; 
+                    }
+                }
+                break;
         }
-
-        if (!(PIND & (1 << 3))) {
-            PORTB = a - b;
-        }
-
-        if (!(PIND & (1 << 4))) {
-            PORTB = a * b;
-        }
-
-        if (!(PIND & (1 << 5))) {
-            if (b != 0) {
-                PORTB = a / b;
-            } else {
-                PORTB = 0xFF;
-                _delay_ms(500);
-            }
-        }
-
-        if (!(PIND & (1 << 6))) {
-            a = 0;
-            b = 0;
-            PORTB = 0;
-        }
+        
+        _delay_ms(50); 
+        
     }
 }
